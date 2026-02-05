59804ed76a17784a0cace81d6a7c920e0cd759db
cio27 <scoobyokelly@gmail.com>
2026-02-05 03:18:06 +0000

    finished palindrome checker

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,27 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+        String cleaned = clean(s);
+        return cleaned.equals(reverse(cleaned));
     }
+    
+    public String clean(String s) {
+        String result = "";
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                result += c;
+            }
+        }
+        return result;
+    }
+    
     public String reverse(String s) {
-        String n = new String();
-        // your code here
+        String n = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            n += s.charAt(i);
+        }
         return n;
     }
 }
